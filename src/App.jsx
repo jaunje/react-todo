2bb188f5eea353c6f717979bf84b0da1eb4fee24
juanje <jaunje@gmail.com>
2024-01-28 16:11:10 +0100

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import "./App.css";
 import ToDoList from "./ToDoList";
 
 function App() {
   return (
     <>
-      <h1>ToDo List</h1>
-      <ToDoList />
+      <div className="container mx-auto flex flex-col justify-center w-full m-10">
+        <h1 className="text-3xl font-bold underline text-red-400 mb-4">
+          ToDo List
+        </h1>
+        <ToDoList />
+      </div>
     </>
   );
 }
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -26,14 +26,18 @@ function ToDoList() {
           onChange={(e) => setTexto(e.target.value)}
           type="text"
         />
-        <button type="submit">Enviar</button>
-        {texto}
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          type="submit"
+        >
+          Enviar
+        </button>
       </form>
 
       <ul>
         {tasks.map((task) => {
           return (
-            <li key={task.id}>
+            <li className="list-disc" key={task.id}>
               {task.id}-{task.name}
             </li>
           );
