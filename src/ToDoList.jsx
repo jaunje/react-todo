26e91de9db1df1f28c7fbf7eeb2aac4dabeaa93e
juanje <jaunje@gmail.com>
2024-02-11 18:45:52 +0100

    fix: add fix to first render

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -69,73 +69,74 @@ function ToDoList() {
       </form>
 
       <ul>
-        {tasks.map((task) => {
-          return (
-            <div
-              key={task.id}
-              role="alert"
-              className="rounded-xl border border-gray-100 bg-white p-4 mb-1"
-            >
-              <div className="flex items-start gap-4">
-                <span
-                  className={`cursor-pointer ${
-                    task.enabled == true
-                      ? "text-green-600 dark:text-green-400"
-                      : "text-red-600 dark:text-red-400"
-                  }`}
-                >
-                  <svg
-                    onClick={() => handleToogleTask(task.id)}
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </span>
-
-                <div className="flex-1">
-                  <strong
-                    className={`block font-bold text-gray-900 ${
-                      task.enabled == false ? "" : "line-through"
+        {tasks &&
+          tasks.map((task) => {
+            return (
+              <div
+                key={task.id}
+                role="alert"
+                className="rounded-xl border border-gray-100 bg-white p-4 mb-1"
+              >
+                <div className="flex items-start gap-4">
+                  <span
+                    className={`cursor-pointer ${
+                      task.enabled == true
+                        ? "text-green-600 dark:text-green-400"
+                        : "text-red-600 dark:text-red-400"
                     }`}
                   >
-                    {task.name}
-                  </strong>
+                    <svg
+                      onClick={() => handleToogleTask(task.id)}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </span>
 
-                  <p className="mt-1 text-sm text-gray-700">ID: {task.id}</p>
+                  <div className="flex-1">
+                    <strong
+                      className={`block font-bold text-gray-900 ${
+                        task.enabled == false ? "" : "line-through"
+                      }`}
+                    >
+                      {task.name}
+                    </strong>
+
+                    <p className="mt-1 text-sm text-gray-700">ID: {task.id}</p>
+                  </div>
+
+                  <button className="text-gray-500 transition hover:text-gray-600">
+                    <span className="sr-only">Dismiss popup</span>
+
+                    <svg
+                      onClick={() => handleRemoveTask(task.id)}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
                 </div>
-
-                <button className="text-gray-500 transition hover:text-gray-600">
-                  <span className="sr-only">Dismiss popup</span>
-
-                  <svg
-                    onClick={() => handleRemoveTask(task.id)}
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </ul>
     </Fragment>
   );
